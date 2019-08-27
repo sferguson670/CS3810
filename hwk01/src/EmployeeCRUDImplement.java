5b3e8f257b33b1720c86e4473da4732f9c33fd3d
sferguson670 <sferguson670@gmail.com>
2019-08-27 16:07:45 -0600

    update done
    update method done and added comments

--- hwk01/src/EmployeeCRUDImplement.java
+++ hwk01/src/EmployeeCRUDImplement.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 
 public class EmployeeCRUDImplement implements EmployeeCRUD {
 
+    //hard coded filename
     static final String EMPLOYEE_FILENAME = "employees.csv";
 
     @Override
@@ -15,6 +16,7 @@ public class EmployeeCRUDImplement implements EmployeeCRUD {
             Scanner in = new Scanner(new FileInputStream(EMPLOYEE_FILENAME));
             while (in.hasNextLine()) {
                 String line = in.nextLine();
+                //scanner breaks line up into employee elements
                 String data[] = line.split(",");
                 int id = Integer.parseInt(data[0]);
                 String name = data[1];
@@ -30,7 +32,7 @@ public class EmployeeCRUDImplement implements EmployeeCRUD {
             //ignoring ...
         }
         if (found) {
-            System.out.println("Employee with the same id already exists");
+            System.out.println("Employee with the same ID already exists.");
         }
         else {
             try {
@@ -55,6 +57,7 @@ public class EmployeeCRUDImplement implements EmployeeCRUD {
                 if (id == key) {
                     String name = data[1];
                     String dep = data[2];
+                    //creates new employee with data array elements
                     Employee employee = new Employee(id, name, dep);
                     in.close();
                     return employee;
@@ -70,12 +73,47 @@ public class EmployeeCRUDImplement implements EmployeeCRUD {
 
     @Override
     public void update (int id, Employee employee) {
-
+        try {
+            Scanner in = new Scanner(new FileInputStream(EMPLOYEE_FILENAME));
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                String data[] = line.split(",");
+                int key = Integer.parseInt(data[0]);
+                if (id == key) {
+                    String name = data[1];
+                    String dep = data[2];
+                    //update name & department
+                    employee.setName(name);
+                    employee.setDepartment(dep);
+                    in.close();
+                }
+            }
+            in.close();
+        }
+        catch (FileNotFoundException ex ) {
+            //ignoring...
+        }
     }
 
     @Override
     public void delete (int id) {
-
+        try {
+            Scanner in = new Scanner(new FileInputStream(EMPLOYEE_FILENAME));
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                String data[] = line.split(",");
+                int key = Integer.parseInt(data[0]);
+                if (id == key) {
+                    String name = data[1];
+                    String dep = data[2];
+                    in.close();
+                }
+            }
+            in.close();
+        }
+        catch (FileNotFoundException ex ) {
+            //ignoring...
+        }
     }
 
     public static void main(String[] args) {
