1f0973170ba56b93aa42027216cd0e58626ab57a
sferguson670 <sferguson670@gmail.com>
2019-09-17 16:14:43 -0600

    added data and updated sql file
    added astronauts data and added more to sql file

---
+++ activity6/astronauts.sql
@@ -0,0 +1,33 @@
+-- astronauts database
+-- my first database
+-- created: 09/12/19
+-- author: Sarah Ferguson
+
+CREATE DATABASE astronauts;
+
+USE astronauts;
+
+DROP TABLE IF EXISTS Astronauts;
+
+-- table astronauts
+CREATE TABLE Astronauts (
+	id			INTEGER PRIMARY KEY AUTO_INCREMENT,
+	lastName	VARCHAR(25),
+	firstName	VARCHAR(20),
+	suffix		VARCHAR(5),
+	gender		CHAR(1),
+	birth		DATE,
+	city		VARCHAR(15),
+	state		VARCHAR(20),
+	country		VARCHAR(15),
+	status		VARCHAR(7),
+	daysInSpace	INTEGER,
+	flights		INTEGER
+);
+
+-- load data
+LOAD DATA INFILE '/Users/sarahdee/Git/CS3810/activity6/astronauts.csv' INTO TABLE Astronauts
+FIELDS TERMINATED BY '.' IGNORE 1 ROWS (lastName, firstName, suffix, gender, birth, city,
+state, country, status, daysInSpace, flights);
+
+-- run queries
