4d4bf3a1d9888a4680b7669a35b9a37f33de8877
sferguson670 <sferguson670@gmail.com>
2019-10-17 15:57:36 -0600

    connection work
    connection worked!

--- hw8/src/main/java/books.java
+++ hw8/src/main/java/books.java
@@ -1,3 +1,20 @@
-public class books {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-}
+    public class books {
+        public static void main(String[] args) throws Exception {
+            String server = "localhost";
+            String database = "xyz";
+            String user = "xyz";
+            String password = "024680";
+            String connectURL = "jdbc:mysql://" + server + "/" + database + "?serverTimezone=UTC&user=" + user + "&password=" + password;
+
+            // connects to the database
+            Connection conn = DriverManager.getConnection(connectURL);
+            System.out.println("Connection to MySQL database " + database + " was successful!");
+
+            // closes the connection
+            System.out.println("Bye!");
+            conn.close();
+        }
+    }
