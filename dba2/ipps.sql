38ca053caf0b42f9fd686435cc1c28b2ef1d6316
Sarah Ferguson <sferguson670@gmail.com>
2019-10-24 10:06:14 -0600

    update sql file
    added more tables and added header

--- dba2/ipps.sql
+++ dba2/ipps.sql
@@ -1,33 +1,51 @@
+-- File: ipps.sql
+-- Database: ipps
+-- Project: Database Assignment #2
+-- Authors: Sarah Ferguson & Ze Liu
+-- Date: 10/22/2019
+
 CREATE DATABASE ipps;
 
 USE ipps;
 
 DROP TABLE IF EXISTS IPPS;
 
-
-
-CREATE TABLE Provide(
-    id    INTEGER PRIMARY KEY, 
-    name  VARCHAR(20),
-    address VARCHAR(50),
-    city   VARCHAR(15),
-    state VARCHAR(20),
-    zip   INTEGER  
-);
-CREATE TABLE Average(
-    total   INTEGER PRIMARY KEY, 
-    coverA INTEGER,
-    totalA INTEGER,
-    medicareA   INTEGER
-);
-CREATE TABLE DRG (
-    code       INTEGER PRIMARY KEY,
-    desc       VARCHAR(60),
-    FOREIGN KEY (id) REFERENCES Provide (id),
-    hospitalSi  VARCHAR(2),
-    hospitalSd  VARCHAR(20),
-    FOREIGN KEY (total) REFERENCES Average (total)
+CREATE TABLE DRGs (
+	code INTEGER PRIMARY KEY,
+	definition VARCHAR(30),
 );
 
-    
+CREATE TABLE Providers (
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(30),
+	streetAddress VARCHAR(30),
+	city VARCHAR(30),
+	state VARCHAR(30),
+	zipCode VARCHAR(30),
+);
 
+CREATE TABLE Charges (
+	totalDischarges INTEGER PRIMARY KEY,
+	avgCoveredCharges DOUBLE,
+	avgTotalPayments DOUBLE,
+	avgMedicarePayments DOUBLE
+);
+
+CREATE TABLE HospitalReferralRegions (
+	code INTEGER PRIMARY KEY,
+	description VARCHAR(30)
+);
+
+CREATE TABLE HospitalVisits (
+	drgCode INTEGER PRIMARY KEY,
+	FOREIGN KEY (drgCode) REFERENCES DRGs (code),
+	providerId INTEGER PRIMARY KEY,
+	FOREIGN KEY (providerId) REFERENCES Providers (id),
+	hospitalReferralRegionCode INTEGER PRIMARY KEY,
+	FOREIGN KEY (hospitalReferralRegionCode) REFERENCES HospitalReferralRegions (code)
+	totalDischarges INTEGER PRIMARY KEY,
+	FOREIGN KEY (totalDischarges) REFERENCES Charges (totalDischarges)
+);
+
+CREATE USER 'test_user' IDENTIFIED BY '032423';
+GRANT ALL ON TABLE ____ TO 'test_user';
