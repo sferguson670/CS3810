912b600cacf6c5f31591d2ae91442b771d31a3c2
Sarah Ferguson <sferguson670@gmail.com>
2019-10-26 20:20:13 -0600

    scanner
    reads file and splits data

--- dba2/src/main/java/IPPS.java
+++ dba2/src/main/java/IPPS.java
@@ -1,10 +1,12 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.*;
 import java.util.*;
 
 public class IPPS {
 
     private static final String CONFIGURATION_FILE = "config.properties";
+    static final String csv_file = "ipps_short.csv";
 
     public static void main(String[] args) throws Exception {
         // open config.properties file
@@ -21,6 +23,44 @@ public class IPPS {
         // connects to the database
         Connection conn = DriverManager.getConnection(connectURL);
         System.out.println("Connection to MySQL database " + database + " was successful!");
+
+        //reads file
+        try {
+            Scanner in = new Scanner(new FileInputStream(csv_file));
+            //skips first line
+            String first = in.nextLine();
+            first = null;
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                String data[] = line.split(",");
+                System.out.println(line);
+                //split data into respective columns
+                String drglong = data[0];
+                String drg[] = drglong.split("-");
+                int drgCode = Integer.parseInt(drg[0].trim());
+                String drgDefinition = drg[1];
+
+                int providerId = Integer.parseInt(data[1]);
+                String providerName = data[3];
+                String providerCity = data[4];
+                String providerState = data[5];
+                int providerZip = Integer.parseInt(data[6]);
+
+                String hospitalReferrallong = data[7].trim();
+                String hospitalReferral[] = hospitalReferrallong.split("-");
+                String hospitalReferralCode = hospitalReferral[0];
+                String hospitalReferralDescription = hospitalReferral[1];
+
+
+                int totalDischarge = Integer.parseInt(data[8]);
+                double averageCoveredCharges = Double.parseDouble(data[9]);
+                double averageTotalPayments = Double.parseDouble(data[10]);
+                double averageMedicarePayments = Double.parseDouble(data[11]);
+                //System.out.println(drgDefinition + " " + providerId + " " + providerName + " " + providerCity + " " + providerState + " " + providerZip  + " " + hospitalReferralDescription + " " + totalDischarge + " " + averageCoveredCharges + " " + averageTotalPayments + " "  + averageMedicarePayments);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
 
         // run a simple query
         Statement stmt = conn.createStatement();
