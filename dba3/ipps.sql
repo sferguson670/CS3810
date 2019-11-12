b872db0a09e24d2d79c967ca06f80c8af9a8f0f9
Sarah Ferguson <sferguson670@gmail.com>
2019-11-12 11:16:04 -0700

    finished dba3
    completed

--- dba3/ipps.sql
+++ dba3/ipps.sql
@@ -1,8 +1,8 @@
 --
 -- CS 3810 - Principles of Database Systems - Fall 2019
 -- DBA03: The "ipps" database
--- Date:
--- Name(s):
+-- Date: 11/11/2019
+-- Name(s): Sarah Ferguson & Ze Liu
 --
 
 -- create database
@@ -64,21 +64,42 @@ LOAD DATA INFILE 'ChargesAndPayments.csv' INTO TABLE ChargesAndPayments FIELDS T
 -- TODO: answer the following queries
 
 -- a) List all diagnostic names in alphabetical order.
+SELECT drgDesc AS "Diagnostic Names" FROM DRGs ORDER BY drgDesc;
 
 -- b) List the names and correspondent states (including Washington D.C.) of all of the providers in alphabetical order (state first, provider name next, no repetition).
+SELECT DISTINCT prvName AS Name, prvState AS State FROM Providers ORDER BY prvState, prvName;
 
 -- c) List the number of (distinct) providers.
+SELECT COUNT(DISTINCT prvName) AS Total FROM Providers;
 
 -- d) List the number of (distinct) providers per state (including Washington D.C.) in alphabetical order (also printing out the state).
+SELECT prvState AS State, COUNT(prvState) AS Total FROM Providers GROUP BY prvState ORDER BY prvState;
 
 -- e) List the number of (distinct) hospital referral regions (HRR).
+SELECT COUNT(DISTINCT hrrName) AS Total FROM HRRs;
 
 -- f) List the number (distinct) of HRRs per state (also printing out the state).
+SELECT hrrState AS State, COUNT(hrrState) AS Total FROM HRRs GROUP BY hrrState ORDER BY hrrState;
 
 -- g) List all of the (distinct) providers in the state of Pennsylvania in alphabetical order.
+SELECT DISTINCT prvName AS Providers FROM Providers WHERE prvState = 'PA' ORDER BY prvName;
 
 -- h) List the top 10 providers (with their correspondent state) that charged  (as described in avgTotalPayments) the most for the diagnose with code 308. Output should display the provider, their state, and the average charged amount in descending order.
+SELECT A.prvName, A.prvState, B.avgTotalPayments FROM Providers A 
+INNER JOIN ChargesAndPayments B ON A.prvId = B.prvId 
+WHERE drgCode = 308 
+ORDER BY avgTotalPayments DESC
+LIMIT 0, 10;
 
 -- i) List the average charges (as described in avgTotalPayments) of all providers per state for the clinical condition with code 308. Output should display the state and the average charged amount per state in descending order (of the charged amount) using only two decimals.
+SELECT A.prvState, TRUNCATE(AVG(B.avgTotalPayments),2) FROM Providers A 
+INNER JOIN ChargesAndPayments B ON A.prvId = B.prvId 
+WHERE drgCode = 308 
+GROUP BY prvState 
+ORDER BY prvState;
 
 -- j) Which hospital and clinical condition pair had the highest difference between the amount charged  (as described in avgTotalPayments) and the amount covered by Medicare  (as described in avgMedicarePayments)?
+SELECT C.prvName, D.drgCode, D.drgDesc, D.gap FROM
+(SELECT A.drgCode, A.drgDesc, B.prvId, B.avgTotalPayments -B.avgMedicarePayments AS gap FROM DRGs A 
+	INNER JOIN ChargesAndPayments B ON A.drgCode = B.drgCode 
+	ORDER BY gap desc limit 1) D INNER JOIN Providers C ON D.prvId = C.prvId;
